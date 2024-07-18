0dcd340b61e270a0deb0f1cbfd72338c14490168
AlexTMFN <75811035+AlexTMFN@users.noreply.github.com>
2024-07-18 17:06:24 +0300

    Update caesarDecrypt.py

--- caesarDecrypt.py
+++ caesarDecrypt.py
@@ -2,10 +2,10 @@ def decrypt(text, step):
 
     result = ""
     for i in range(len(text)):
-        decryptedChar = ((ord(text[i]) - 97 + step) % 26) + 97
+        decryptedChar = ((ord(text[i]) - 97 - step) % 26) + 97
         result = result + chr(decryptedChar)
     return result
 
-text = "ynkooejcpdanqxeykjrbdofgkq"
-print({decrypt(text, 4)})
+text = "wklvzdvxvhgwrghfubswdfwiiurpslfrFWI"
+print({decrypt(text, 3)})
 
